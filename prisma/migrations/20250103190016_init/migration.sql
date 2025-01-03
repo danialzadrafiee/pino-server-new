7f82ee57533a9a0e97c3cc4ceb334ea388d0d773
danialzadrafiee <leylasarafrezai@gmail.com>
2025-01-03 22:30:31 +0330

    update

--- prisma/migrations/20250103190016_init/migration.sql
+++ prisma/migrations/20250103190016_init/migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE `User` (
     `referrer_id` INTEGER NULL,
     `referral_code` VARCHAR(191) NOT NULL,
     `referral_modal_watched` BOOLEAN NULL DEFAULT false,
-    `telegram_id` BIGINT NOT NULL,
+    `telegram_id` VARCHAR(191) NOT NULL,
     `telegram_username` VARCHAR(191) NULL,
     `telegram_firstname` VARCHAR(191) NULL,
     `telegram_lastname` VARCHAR(191) NULL,
@@ -21,7 +21,6 @@ CREATE TABLE `User` (
     `updated_at` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_referral_code_key`(`referral_code`),
-    UNIQUE INDEX `User_telegram_id_key`(`telegram_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -30,7 +29,7 @@ CREATE TABLE `Referral` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `referrer_id` INTEGER NOT NULL,
     `referred_id` INTEGER NOT NULL,
-    `telegram_id` BIGINT NOT NULL,
+    `telegram_id` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
--- prisma/seeds/user.seed.ts
+++ prisma/seeds/user.seed.ts
@@ -1,41 +1,11 @@
 import { Prisma } from '@prisma/client';
 
 export async function seedUser(prisma: Prisma.TransactionClient) {
-  await prisma.user.upsert({
-    where: {
-      telegram_id: 123,
-    },
-    update: {},
-    create: {
-      telegram_id: 123,
+  await prisma.user.create({
+    data: {
+      telegram_id: '123',
       referrer_id: null,
-      referral_code: "11111111",
-      telegram_username: 'demo_user',
-      telegram_firstname: 'Demo',
-      telegram_lastname: 'User',
-      cricket_balance: 0,
-      this_match_apple_earning: 0,
-      previous_match_apple_earning: 0,
-      direct_referral_count: 0,
-      downline_referral_count: 0,
-      apple_balance: 0,
-      apple_per_second: 0,
-      pets: [],
-      referral_modal_watched: false,
-      last_heartbeat: new Date(),
-      created_at: new Date(),
-      updated_at: new Date(),
-    },
-  });
-  await prisma.user.upsert({
-    where: {
-      telegram_id: 124,
-    },
-    update: {},
-    create: {
-      telegram_id: 124,
-      referrer_id: null,
-      referral_code: "22222222",
+      referral_code: '11111111',
       telegram_username: 'demo_user',
       telegram_firstname: 'Demo',
       telegram_lastname: 'User',
